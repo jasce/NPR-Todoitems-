a8ab20cec11966ef85b62c0a8a3001362a382f8b
Jasmeet Singh <jascee.gtbit@gmail.com>
2017-07-02 19:14:35 +0530

    Port Issue solved for heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
 var database = require('./models/database');
-var serverPort = 8080;
+var serverPort = process.env.PORT || 8080;
 
 // swaggerRouter configuration
 var options = {
